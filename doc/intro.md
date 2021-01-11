caa173db099ad277aeaee8fe158eb5e1cf8dc74c
Jéferson Decker <deckerjeferson@gmail.com>
2021-01-11 14:45:04 -0300

    First Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to finger-snapper
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/finger_snapper/core_test.clj
@@ -0,0 +1,7 @@
+(ns finger-snapper.core-test
+  (:require [clojure.test :refer :all]
+            [finger-snapper.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
